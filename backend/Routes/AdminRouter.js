2e9c61b870df6d2a275ef487c6e99daf8915b14f
Muhannad Salkini <67662481+muhannadsalkini@users.noreply.github.com>
2024-05-20 22:27:46 +0300

    Delete backend directory

--- backend/Routes/AdminRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { createAdmin, getAdmin } = require("../Controllers/AdminController");
-
-router.get("/getAdmin", getAdmin);
-router.post("/create", createAdmin);
-
-module.exports = router;
