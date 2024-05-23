cf767adeecb84e37d5c738128a2e7d129e167d8c
MUHANNAD SALKINI <mohanad.salkini@gmail.com>
2024-05-23 19:07:33 +0300

    Added dotenv config, updated port variable to use fallback value, and updated console log message to include dynamic port number

--- server.js
+++ server.js
@@ -3,19 +3,22 @@ const mongoose = require("mongoose");
 const routes = require("./Routes/index");
 const cors = require("cors");
 
+// Load environment variables from .env file
+require("dotenv").config();
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 const mongoUrl = process.env.PRODUCTION_MONGO_URL;
-const port = process.env.DEV_PROT;
+const port = process.env.DEV_PORT || 3060; // Fallback to 3060 if DEV_PORT is not defined
 
 mongoose
   .connect(mongoUrl)
   .then(() => {
-    app.listen(3060, () => {
-      console.log("Server is running on port 3060");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
     });
   })
   .catch((err) => {
